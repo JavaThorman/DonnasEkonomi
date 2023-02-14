48886049c22bc74f67267c6804be9d08105fe4ab
JavaThorman <120687540+JavaThorman@users.noreply.github.com>
2023-02-14 14:50:13 +0100

    ok

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Skapa variabler för alla inkomster och utgifter
+        double allowance = 3652;
+        double loan = 8400;
+        double rent = 6500;
+        double food = 2000;
+        double bus = 795;
+        double goodies = 200;
+        double clothes = 800;
+        double other = 1000;
+
+// Skapa variabler för summan av inkomster och utgifter
+        double sumIncome = allowance + loan;
+        double sumExpenses = rent + food + bus + goodies + clothes + other;
+
+// Skapa variabel för överskott
+        double surplus = sumIncome - sumExpenses;
+
+    }
+}
